23c781d2b9805ffd40b8ffdbaf9aaa824034db95
Jerli <jerli2004@github.com>
2025-12-22 19:15:12 +0530

    Add website files

--- hustlecurrency/settings.py
+++ hustlecurrency/settings.py
@@ -15,8 +15,14 @@ import os
 from decouple import config
 
 import dj_database_url
+
+
 DATABASES = {
-    'default': dj_database_url.parse(config('DATABASE_URL'), conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.parse(
+        config('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
