51c8ae2734c5fe20e7b5bc4b7bd5c989ddde7b99
Jerli <jerli2004@github.com>
2025-12-22 21:12:39 +0530

    Fix static file structure

--- hustlecurrency/settings.py
+++ hustlecurrency/settings.py
@@ -134,11 +134,13 @@ USE_TZ = True
 
 # STATIC FILES
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'store', 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'store', 'static'),
+# ]
+
+STATIC_ROOT=BASE_DIR / "static"
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
