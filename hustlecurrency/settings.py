a77fc75103f65222eae064c6f8ad233ba6f3776f
Risheb.s <rishebs123456@gmail.com>
2025-12-22 23:49:46 +0530

    3rd commit

--- hustlecurrency/settings.py
+++ hustlecurrency/settings.py
@@ -140,7 +140,8 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # MEDIA FILES
 MEDIA_URL = '/media/'
