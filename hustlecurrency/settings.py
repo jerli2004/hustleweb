cdce206e926b097f5b838413153b4e7618b7b4d3
Jerli <jerli2004@github.com>
2025-12-22 20:42:34 +0530

    Add website files

--- hustlecurrency/settings.py
+++ hustlecurrency/settings.py
@@ -136,11 +136,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-MEDIA_URL='media/'
-MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Where your dev static files are
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')    # Where collectstatic will copy them
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'store', 'static'),
+]
+
+
+MEDIA_URL='media/' 
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+
 
 
 
