788764c4d222df58f2c8fb3596c0670ad106b1f8
Jerli <jerli2004@github.com>
2025-12-22 20:34:52 +0530

    Add website files

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,8 @@
 # urls.py
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -40,3 +42,7 @@ urlpatterns = [
 
     
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
