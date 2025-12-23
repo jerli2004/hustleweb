81e40dd65d494832faeda1496ffcd31a23ed18aa
Jerli <jerli2004@github.com>
2025-12-23 07:04:20 +0530

    Add website files

--- hustlecurrency/settings.py
+++ hustlecurrency/settings.py
@@ -46,7 +46,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store'
+    'store',
+    'cloudinary',
+    'cloudinary_storage',
 ]
 
 MIDDLEWARE = [
