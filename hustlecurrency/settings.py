f9f348109e2a19a2f9a06a265682963bdef98aae
Jerli <jerli2004@github.com>
2025-12-24 19:52:26 +0530

    Add website files

--- hustlecurrency/settings.py
+++ hustlecurrency/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY ='uW9TJJvVoKL3lp06JqqsEsNY7SYpFRQHZdY_OwsPiniqisLrHrDmFWHR7PhDMDYmQGM
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 
